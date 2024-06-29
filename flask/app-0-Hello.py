b8d377df4ad7f9baec374c5f2782d9309d38b060
Anthony Perez <anthony.perez101417@gmail.com>
2024-06-28 20:07:40 -0400

    Submitting Assignment W7D4

---
+++ flask/app-0-Hello.py
@@ -0,0 +1,13 @@
+#Run this at the start of every terminal session:
+#python -m venv venv
+#URL: http://127.0.0.1:5000/
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return f'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
